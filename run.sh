ca7a9a654e22f0fbba1f736e7456dc2499e2fcb7
Mineru98 <rmstjr1030@naver.com>
2021-02-07 13:24:59 +0900

    Update Code

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-set -a && JUPYTER_TOKEN="@token" && set +a
-jupyter notebook --allow-root --ip=0.0.0.0 --port=80 --NotebookApp.token=$JUPYTER_TOKEN &
+set -a && JUPYTER_TOKEN="@dos664500" && set +a
+jupyter notebook --allow-root --ip=0.0.0.0 --port=8080 --NotebookApp.token=$JUPYTER_TOKEN &
